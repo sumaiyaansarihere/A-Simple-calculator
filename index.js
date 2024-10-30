d065f82556b625d6885d411ba53eb4633f5dbbb0
sumaiyaansarihere <sumaiyaansari423@gmail.com>
2024-10-30 14:59:51 +0500

    Add files via upload
    A simple Calculator Using Typescript

---
+++ index.js
@@ -0,0 +1,47 @@
+import inquirer from "inquirer";
+async function calculate() {
+    const answer = await inquirer.prompt([
+        {
+            type: "number",
+            name: "number1",
+            message: "Enter the first number:",
+        },
+        {
+            type: "number",
+            name: "number2",
+            message: "Enter the second number:",
+        },
+        {
+            type: "list",
+            name: "operator",
+            message: "Select an operator:",
+            choices: ["+", "-", "*", "/"]
+        }
+    ]);
+    const { number1, number2, operator } = answer;
+    if (number1 !== undefined && number2 !== undefined && operator) {
+        let result = 0;
+        if (operator === "+") {
+            result = number1 + number2;
+        }
+        else if (operator === "-") {
+            result = number1 - number2;
+        }
+        else if (operator === "*") {
+            result = number1 * number2;
+        }
+        else if (operator === "/") {
+            result = number2 !== 0 ? number1 / number2 : NaN; // Avoid division by zero
+        }
+        if (isNaN(result)) {
+            console.log("Error: Division by zero is not allowed.");
+        }
+        else {
+            console.log("Your result is: " + result);
+        }
+    }
+    else {
+        console.log("Please provide valid inputs.");
+    }
+}
+calculate();
